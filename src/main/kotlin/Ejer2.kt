0fc6a84913b112326b3eeae9ebac39401add4ce1
diurno <davivimix96@gmail.com>
2026-09-14 13:35:02 +0200

    probando 1er ejercicio

---
+++ src/main/kotlin/Ejer2.kt
@@ -0,0 +1,12 @@
+package org.example
+
+//2. Un método que reciba un entero por parámetro e imprima el texto “El resultado es:”
+// y el parámetro recibido. Este método no devolverá nada
+
+fun texto(num: Int){
+    println("El resultado es")
+}
+
+fun main() {
+    texto(2)
+}
---
+++ src/main/kotlin/Ejer3.kt
@@ -0,0 +1,10 @@
+package org.example
+
+//3. Un método que dado un entero,devuelva el sumatorio de cero a ese número.
+// Por ejemplo, si se pasa el 6, el resultado será 0 +1 +2 +3 +4 +5+ 6.
+
+fun sumatorio(num: Int,num2: Int) : Int = num + num2
+
+fun main(){
+    println(sumatorio(0,1))
+}
