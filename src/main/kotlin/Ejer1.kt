5a3c288e5a1b65c8aad8563da149c08a817daece
diurno <davivimix96@gmail.com>
2026-09-14 13:15:44 +0200

    probando 1er ejercicio

---
+++ src/main/kotlin/Ejer1.kt
@@ -0,0 +1,18 @@
+package org.example
+
+//1. Un método que calcule la suma de sus dos parámetros enteros y devuelva el resultado
+
+fun calcularSuma(n1: Int, n2: Int): Int = n1 + n2
+
+//Otra forma de hacer
+fun calcularSuma2(n1: Int, n2: Int): Int
+{
+    return n1 +n2
+}
+fun main(){
+    println(calcularSuma(4,4))
+}
+
+
+
+
